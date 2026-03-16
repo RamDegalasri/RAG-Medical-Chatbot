e7507890c4574cfaff65ad08459759b65de9d08e
sriram-degala <ram.degala2025@gmail.com>
2026-03-16 17:51:13 -0500

    app commit, data commit

---
+++ app/config/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    DATAPATH = "data/"
+    DB_FAISS_PATH = ""
+    CHUNK_SIZE = 500
+    CHUNK_OVERLAP = 50
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name = "Medical RAG Chatbot",
+    version = "1.0.0",
+    author = "Sriram Degala",
+    packages = find_packages(),
+    install_requires = requirements,
+)
